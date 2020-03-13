395e1dd2c91450d327b3e0e5db3d5c7fd712e840
Phuwanatm <33540495+Phuwanatm@users.noreply.github.com>
2020-03-13 15:57:56 +0700

    Quiz ยังไม่เสร็จ

---
+++ mavenproject1/src/main/java/com/mycompany/mavenproject1/Main.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.mavenproject1;
+
+/**
+ *
+ * @author Lenovo Lab
+ */
+public class Main {
+    public double sum(double n1, double n2, double n3, double n4, double n5, double divide ){
+        return (n1 + n2 + n3 + n4 + n5) / divide;
+    }
+}
